69c1e675d71c7e77d933e39169234b4f7aa10dcc
Louraine Macario de Lima <114750948+lourainemacario18@users.noreply.github.com>
2024-04-25 17:19:38 -0300

    Exercícios com Operadores

---
+++ Python/operadores_comparacao.py
@@ -0,0 +1,9 @@
+saldo = 200
+saque = 200
+
+print(saldo == saque) # igual 
+print(saldo != saque) # diferente de
+print(saldo > saque) # maior que
+print(saldo >= saque) # maior ou igual
+print(saldo < saque) # menor
+print(saldo <= saque) # menor ou igual
