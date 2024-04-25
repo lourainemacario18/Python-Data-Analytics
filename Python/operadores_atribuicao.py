05aa8c25d9116a2281a6d8337a0361989beb5c80
Louraine Macario de Lima <114750948+lourainemacario18@users.noreply.github.com>
2024-04-25 17:17:30 -0300

    Exercícios com Operadores

---
+++ Python/operadores_atribuicao.py
@@ -0,0 +1,26 @@
+saldo = 500 #atribuicao simples
+
+print(saldo)
+
+saldo = 200 #substituicao print(saldo)
+
+saldo += 10 #adicao 
+print(saldo)
+
+saldo -= 5 #subtracao 
+print(saldo)
+
+saldo //= 2 #divisao inteira
+print(saldo)
+
+saldo /= 2 #divisao 
+print(saldo)
+
+saldo *= 10 #multiplicacao
+print(saldo)
+
+saldo %= 3 #modulo
+print(saldo)
+
+saldo **= 2 #exponenciacao
+print(saldo)
